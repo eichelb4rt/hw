62d3410f0d02780449e4726f16b8905b9dc2e410
eichelb4rt <maurice.wenig@gmail.com>
2022-05-10 13:50:59 +0200

    animation

--- S8_SS2022/ECDS/02/SIR.py
+++ S8_SS2022/ECDS/02/SIR.py
@@ -1,35 +1,69 @@
-import math
+import matplotlib.animation as animation
 import numpy as np
 import matplotlib.pyplot as plt
 from explicit_euler import explicit_euler
+from bezier import bezier
+
+N = 1000
+Y_0 = np.array([
+    997,
+    3,
+    0,
+    0
+])
+T = np.arange(0, 100, 0.01)
+# animation bezier stuff
+P1 = np.array([
+    0.5,
+    0.01,
+    0.3
+])
+P2 = np.array([
+    1,
+    0.01,
+    0.005
+])
+P3 = np.array([
+    0.5,
+    0.5,
+    0.005
+])
+P4 = P1
+ANIMATION_STEPS = 50
+PARAMS = bezier(P1, P2, P3, P4, steps=ANIMATION_STEPS)
+
+
+def approximate(alpha, beta, gamma):
+    # S: 0, I: 1, R: 2, H: 3
+    f = lambda t_i, y_i: np.array([
+        - alpha * y_i[0] * y_i[1] / N,
+        alpha * y_i[0] * y_i[1] / N - gamma * y_i[1] - beta * y_i[1],
+        beta * y_i[1],
+        gamma * y_i[1]
+    ])
+    return explicit_euler(f, Y_0, T)
 
 
 def main():
-    ALPHA = 0.5
-    BETA = 0.01
-    GAMMA = 0.04
-    N = 1000
+    fig, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [1, 3]})
+    max_param = np.max(PARAMS)
 
-    y_0 = np.array([
-        997,
-        3,
-        0,
-        0
-    ])
-    t = np.arange(0, 100, 0.01)
-    # S: 0, I: 1, R: 2, H: 3
-    f = lambda t_i, y_i: np.array([
-        - ALPHA * y_i[0] * y_i[1] / N,
-        ALPHA * y_i[0] * y_i[1] / N - GAMMA * y_i[1] - BETA * y_i[1],
-        BETA * y_i[1],
-        GAMMA * y_i[1]
-    ])
-    approximation = explicit_euler(f, y_0, t)
-    plt.plot(t, approximation[:, 0], label="S")
-    plt.plot(t, approximation[:, 1], label="I")
-    plt.plot(t, approximation[:, 2], label="R")
-    plt.plot(t, approximation[:, 3], label="H")
-    plt.legend()
+    def animate(i):
+        ALPHA, BETA, GAMMA = PARAMS[i]
+        # animate params
+        ax0.clear()
+        ax0.set_ylim(0, max_param * 1.1)
+        ax0.bar(["alpha", "beta", "gamma"], [ALPHA, BETA, GAMMA], color=['orange', 'green', 'black'])
+        ax0.tick_params(labelrotation=45)
+        # animate approximation
+        approximation = approximate(ALPHA, BETA, GAMMA)
+        ax1.clear()
+        ax1.plot(T, approximation[:, 0], label="S", color='blue')
+        ax1.plot(T, approximation[:, 1], label="I", color='orange')
+        ax1.plot(T, approximation[:, 2], label="R", color='green')
+        ax1.plot(T, approximation[:, 3], label="H", color='black')
+        ax1.legend()
+    ani = animation.FuncAnimation(fig, animate, frames=ANIMATION_STEPS, interval=10)
     plt.show()
 
 
--- S8_SS2022/ECDS/02/bezier.py
+++ S8_SS2022/ECDS/02/bezier.py
@@ -2,25 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def lerp(A, B, t):
-    return t * A + (1 - t) * B
+def lerp(a, b, t):
+    return t * a + (1 - t) * b
 
 
-def bezier_t(P1, P2, P3, P4, t):
-    PP1 = lerp(P1, P2, t)
-    PP2 = lerp(P2, P3, t)
-    PP3 = lerp(P3, P4, t)
-    PPP1 = lerp(PP1, PP2, t)
-    PPP2 = lerp(PP2, PP3, t)
-    return lerp(PPP1, PPP2, t)
+def bezier_t(p1, p2, p3, p4, t):
+    pp1 = lerp(p1, p2, t)
+    pp2 = lerp(p2, p3, t)
+    pp3 = lerp(p3, p4, t)
+    ppp1 = lerp(pp1, pp2, t)
+    ppp2 = lerp(pp2, pp3, t)
+    return lerp(ppp1, ppp2, t)
 
 
-def bezier(P1, P2, P3, P4, steps=101):
-    points = np.empty((steps, len(P1)))
-    # include endpoint 1
-    ts = np.arange(0, 1 + 1 / (steps - 1), 1 / (steps - 1))
+def bezier(p1, p2, p3, p4, steps=101):
+    points = np.empty((steps, len(p1)))
+    ts = np.linspace(0, 1, steps)
     for i, t in enumerate(ts):
-        points[i] = bezier_t(P1, P2, P3, P4, t)
+        points[i] = bezier_t(p1, p2, p3, p4, t)
     return points
 
 
